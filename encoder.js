2d2b49f4977afa88b3a8e7685750f963e5e84ead
Jared Diaz <Jared.Diaz.1890@gmail.com>
2016-10-21 11:09:50 -0700

    add radio buttons to html

--- encoder.js
+++ encoder.js
@@ -1,6 +1,36 @@
-$("#inputText").keydown(function(e) {
-  var inputText = document.getElementById("inputText").value;
-  document.getElementById("textArea").innerHTML = inputText;
-});
+caesar = {
+32: " ",
+65: "z",
+66: "a",
+67: "b",
+68: "c",
+69: "d",
+70: "e",
+71: "f",
+72: "g",
+73: "h",
+74: "i",
+75: "j",
+76: "k",
+77: "l",
+78: "m",
+79: "n",
+80: "o",
+81: "p",
+82: "q",
+83: "r",
+84: "s",
+85: "t",
+86: "u",
+87: "v",
+88: "w",
+89: "x",
+90: "y",
+}
 
-$("")
+$("#inputText").keydown(function(e)
+
+$("input[type = 'radio']").click(function(e) {
+  $("input.checked").prop('checked', true);
+  $(this).prop('checked', false);
+});
